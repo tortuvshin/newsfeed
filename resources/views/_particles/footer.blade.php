36711ba3fa27a8352197683cadc0ce7659202324
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-23 02:14:51 +0800

    main header footer index blades finish

--- resources/views/_particles/footer.blade.php
+++ resources/views/_particles/footer.blade.php
@@ -3,17 +3,12 @@
     <div class="wrapper">
 
         <div class="row">
-
+            
             <div class="col-md-3 col-sm-3 widget-area-11">
 
                 <div class="widget widget_text">
-                    <div id="footer-logo-image">
-                        <a href="index.html">
-                            <img src="newsfeed/placeholders/footer-logo.png" alt="">
-                        </a>
-                    </div>
-                    <p>Proin eu sapien non tortor mattis auctor ac sit amet justo. Aliquam pellentesque odio quis eleifend aliquet. In id sodales dui. Pellentesque ac est risus. Vestibulum elementum elementum massa, ultrices feugiat magna aliquam non.
-                        Nullam eu libero quis</p>
+                    <div id="footer-logo-image"><a href="index.html"><img src="placeholders/footer-logo.png" alt=""></a></div>
+                    <p>Proin eu sapien non tortor mattis auctor ac sit amet justo. Aliquam pellentesque odio quis eleifend aliquet. In id sodales dui. Pellentesque ac est risus. Vestibulum elementum elementum massa, ultrices feugiat magna aliquam non. Nullam eu libero quis</p>
                 </div>
                 <!-- end:widget_text -->
 
@@ -21,19 +16,13 @@
 
                     <p class="contact-address">Pitampura, New Delhi, Delhi, India</p>
 
-                    <p class="contact-phone">
-                        <span>Call:&nbsp;</span>
-                        <a href="callto:323244343">+61 (0) 3 8376 6286</a>
-                    </p>
+                    <p class="contact-phone"><span>Call:&nbsp;</span><a href="callto:323244343">+61 (0) 3 8376 6286</a></p>
 
-                    <p class="contact-email">
-                        <span>Email:&nbsp;</span>
-                        <a href="mailto:contact@company.com">contact@company.com</a>
-                    </p>
-
+                    <p class="contact-email"><span>Email:&nbsp;</span><a href="mailto:contact@company.com">contact@company.com</a></p>
+                    
                 </div>
                 <!-- end:punica-contact-info-widget -->
-
+                
             </div>
             <!-- end:col-md-3 -->
 
@@ -41,19 +30,13 @@
 
                 <div class="widget punica-article-list-4-widget">
 
-                    <div class="widget-title widget-title-s5 clearfix">
-                        <h2>
-                            <span>Recent posts</span>
-                        </h2>
-                    </div>
+                    <div class="widget-title widget-title-s5 clearfix"><h2><span>Recent posts</span></h2></div>
 
                     <ul class="clearfix">
                         <li>
                             <article class="entry-item clearfix">
                                 <div class="entry-thumb pull-left">
-                                    <a href="#">
-                                        <img src="newsfeed/placeholders/post-image/post-19.jpg" alt="">
-                                    </a>
+                                    <a href="#"><img src="placeholders/post-image/post-19.jpg" alt=""></a>
                                 </div>
                                 <!-- end:entry-thumb -->
                                 <div class="entry-content">
@@ -72,9 +55,7 @@
                                         <!-- end:entry-author -->
                                     </header>
 
-                                    <h6 class="entry-title">
-                                        <a href="#">Suspected Suicide Bomber Kills 45 on Pakistani-Indian Border</a>
-                                    </h6>
+                                    <h6 class="entry-title"><a href="#">Suspected Suicide Bomber Kills 45 on Pakistani-Indian Border</a></h6>
                                 </div>
                                 <!-- end:entry-content -->
                             </article>
@@ -84,9 +65,7 @@
                         <li>
                             <article class="entry-item clearfix">
                                 <div class="entry-thumb pull-left">
-                                    <a href="#">
-                                        <img src="newsfeed/placeholders/post-image/post-18.jpg" alt="">
-                                    </a>
+                                    <a href="#"><img src="placeholders/post-image/post-18.jpg" alt=""></a>
                                 </div>
                                 <!-- end:entry-thumb -->
                                 <div class="entry-content">
@@ -105,9 +84,7 @@
                                         <!-- end:entry-author -->
                                     </header>
 
-                                    <h6 class="entry-title">
-                                        <a href="#">Suspension is a feminist issue</a>
-                                    </h6>
+                                    <h6 class="entry-title"><a href="#">Suspension is a feminist issue</a></h6>
                                 </div>
                                 <!-- end:entry-content -->
                             </article>
@@ -117,9 +94,7 @@
                         <li>
                             <article class="entry-item clearfix">
                                 <div class="entry-thumb pull-left">
-                                    <a href="#">
-                                        <img src="newsfeed/placeholders/post-image/post-11.jpg" alt="">
-                                    </a>
+                                    <a href="#"><img src="placeholders/post-image/post-11.jpg" alt=""></a>
                                 </div>
                                 <!-- end:entry-thumb -->
                                 <div class="entry-content">
@@ -138,19 +113,17 @@
                                         <!-- end:entry-author -->
                                     </header>
 
-                                    <h6 class="entry-title">
-                                        <a href="#">Privately educated gain £1,500 salary premium</a>
-                                    </h6>
+                                    <h6 class="entry-title"><a href="#">Privately educated gain £1,500 salary premium</a></h6>
                                 </div>
                                 <!-- end:entry-content -->
                             </article>
                             <!-- end:entry-item -->
                         </li>
                     </ul>
-
+                    
                 </div>
                 <!-- end:punica-article-list-4-widget -->
-
+                
             </div>
             <!-- end:col-md-3 -->
 
@@ -158,69 +131,19 @@
 
                 <div class="widget punica-flickr-widget">
 
-                    <div class="widget-title widget-title-s5 clearfix">
-                        <h2>
-                            <span>On Flickr</span>
-                        </h2>
-                    </div>
+                    <div class="widget-title widget-title-s5 clearfix"><h2><span>On Flickr</span></h2></div>
 
                     <div class="widget-content">
                         <div class="flickr-wrap clearfix" data-user="129289431@N06">
-                            <ul class="clearfix list-unstyled">
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/154277246@N03/24394192827/" title="All India Tour" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4641/24394192827_ee61aa08ea_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4641/24394192827_ee61aa08ea_m.jpg" alt="All India Tour" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/160754347@N06/24394194857/" title="20171223_173725" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4597/24394194857_292c185afb_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4597/24394194857_292c185afb_m.jpg" alt="20171223_173725" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/russellstreet/24394195347/" title="Grand Canyon Views" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4729/24394195347_a974506094_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4729/24394195347_a974506094_m.jpg" alt="Grand Canyon Views" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/156310773@N04/25389759288/" title="imsi20171224054150" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4731/25389759288_ce4254869f_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4731/25389759288_ce4254869f_m.jpg" alt="imsi20171224054150" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/ngochoaitn/25389759518/" title="2017-12-24_03-41-44" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4589/25389759518_fc30a55d4f_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4589/25389759518_fc30a55d4f_m.jpg" alt="2017-12-24_03-41-44" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/149698601@N07/25389759908/" title="image" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4647/25389759908_9d238c609b_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4647/25389759908_9d238c609b_m.jpg" alt="image" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/vanillacakesandcookies/25389760618/" title="2017-12-24_03-41-53" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4646/25389760618_8dd7bfd0e0_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4646/25389760618_8dd7bfd0e0_m.jpg" alt="2017-12-24_03-41-53" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/155891044@N05/25389761188/" title="imsi20171224054159" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4690/25389761188_28c9fbb69f_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4690/25389761188_28c9fbb69f_m.jpg" alt="imsi20171224054159" style="display: none;">
-                                    </a>
-                                </li>
-                                <li class="flickr-badge-image">
-                                    <a target="blank" href="http://www.flickr.com/photos/161579948@N08/25389762378/" title="636497269240290846" class="imgLiquid imgLiquid_bgSize imgLiquid_ready" style="background-image: url(&quot;http://farm5.staticflickr.com/4593/25389762378_14f1f55478_m.jpg&quot;); background-size: cover; background-position: center center; background-repeat: no-repeat;">
-                                        <img src="http://farm5.staticflickr.com/4593/25389762378_14f1f55478_m.jpg" alt="636497269240290846" style="display: none;">
-                                    </a>
-                                </li>
-                            </ul>
+                            <ul class="clearfix list-unstyled"></ul>
                         </div>
                         <!--flickr-wrap-->
                     </div>
                     <!-- widget-content -->
-
+                    
                 </div>
                 <!-- punica-flickr-widget -->
-
+                
             </div>
             <!-- end:col-md-3 -->
 
@@ -228,14 +151,10 @@
 
                 <div class="widget punica-newsletter-widget">
 
-                    <div class="widget-title widget-title-s5 clearfix">
-                        <h2>
-                            <span>Newsletter</span>
-                        </h2>
-                    </div>
-
+                    <div class="widget-title widget-title-s5 clearfix"><h2><span>Newsletter</span></h2></div>
+                    
                     <form class="newsletter-form clearfix" method="post" action="processNewsletterForm.php">
-
+                        
                         <p class="input-email clearfix">
                             <input type="text" size="40" class="email" value="" name="email" onblur="if(this.value=='')this.value=this.defaultValue;" onfocus="if(this.value==this.defaultValue)this.value='';">
                             <input type="submit" class="submit" value="Subcribe">
@@ -254,61 +173,42 @@
                     <p>We accept the following payment:</p>
 
                     <ul class="clearfix">
-                        <li>
-                            <a href="#">
-                                <img src="newsfeed/images/icons/visa-icon.png" alt="">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src="newsfeed/images/icons/master-card-icon.png" alt="">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src="newsfeed/images/icons/discover-icon.png" alt="">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src="newsfeed/images/icons/american-express-icon.png" alt="">
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                <img src="newsfeed/images/icons/paypal-icon.png" alt="">
-                            </a>
-                        </li>
+                        <li><a href="#"><img src="images/icons/visa-icon.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/icons/master-card-icon.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/icons/discover-icon.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/icons/american-express-icon.png" alt=""></a></li>
+                        <li><a href="#"><img src="images/icons/paypal-icon.png" alt=""></a></li>
                     </ul>
-
+                    
                 </div>
                 <!-- end:punica-payment-widget -->
-
+                
             </div>
             <!-- end:col-md-3 -->
 
         </div>
         <!-- end:row -->
-
+        
     </div>
     <!-- end:wrapper -->
 
     <div class="mask"></div>
-
+    
 </div>
+<!-- end:bottom-sidebar -->
 
 <footer id="punica-page-footer">
 
-<div class="wrapper clearfix">
+    <div class="wrapper clearfix">
 
-    <p id="copyright" class="pull-left">Copyright 2014 punica Premium Magazine Theme All Right Reserved.</p>
+        <p id="copyright" class="pull-left">Copyright 2014 punica Premium Magazine Theme All Right Reserved.</p>
 
-    <p id="back-top" class="pull-right" style="display: block;">
-        <a href="#top" class="clearfix">Back to top</a>
-    </p>
-    <!-- end:back-top -->
-
-</div>
-<!-- end:wrapper -->
-
+        <p id="back-top" class="pull-right">
+            <a href="#top" class="clearfix">Back to top</a>
+        </p>
+        <!-- end:back-top -->
+        
+    </div>
+    <!-- end:wrapper -->   
+    
 </footer>
--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,111 +1,76 @@
 <!DOCTYPE html>
-<html lang="{{ Lang::getLocale() }}">
+<html lang="en">
 <head>
-    <title>@yield('head_title', getcong('sitetitle'))</title>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="@yield('head_description', getcong('sitemetadesc'))" />
+    <meta charset="utf-8">
+    <title>UnicMag - HTML5 Responsive Template</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <link rel="stylesheet" href="css/bootstrap.min.css" media="all" />
+    <link rel="stylesheet" href="css/font-awesome.min.css" media="all" />
+    <link rel="stylesheet" href="css/superfish.css" media="all" />
+    <link rel="stylesheet" href="css/owl.carousel.css" media="all" />
+    <link rel="stylesheet" href="css/owl.theme.css" media="all" />
+    <link rel="stylesheet" href="css/jquery.navgoco.css"/>
+    <link rel="stylesheet" href="css/flexslider.css"/>
+    <link rel="stylesheet" href="css/color-options.css" media="all" />
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="demo.css">
+    <link rel="stylesheet" href="css/responsive.css"/>
+    <script src="js/modernizr.custom.js"></script>
 
-    <meta property="og:type" content="article" />
-    <meta property="og:title" content="@yield('head_title',  getcong('sitetitle'))" />
-    <meta property="og:description" content="@yield('head_description', getcong('sitemetadesc'))" />
-    
-    <meta property="og:image" content="@yield('head_image', url('/assets/img/flogo.png'))" />
-    <meta property="og:url" content="@yield('head_url', url())" />
 
-    <meta name="twitter:image" content="@yield('head_image', url('/assets/img/logo.png'))" />
-    <meta name="twitter:card" content="summary">
-    <meta name="twitter:url" content="@yield('head_url', url())">
-    <meta name="twitter:title" content="@yield('head_title',  getcong('sitetitle'))">
-    <meta name="twitter:description" content="@yield('head_description', getcong('sitemetadesc'))">
-    
-    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
-    <link href="{{ url('/assets/img/favicon.png') }}" rel="shortcut icon" type="image/x-icon" />
+    <!-- Color Switch -->
+    <link rel="stylesheet" href="css/skin/red.css" type="text/css" id="colors" />
 
-    <link rel="stylesheet" href="newsfeed/css/bootstrap.min.css" media="all">
-    <link rel="stylesheet" href="newsfeed/css/font-awesome.min.css" media="all">
-    <link rel="stylesheet" href="newsfeed/css/superfish.css" media="all">
-    <link rel="stylesheet" href="newsfeed/css/owl.carousel.css" media="all">
-    <link rel="stylesheet" href="newsfeed/css/owl.theme.css" media="all">
-    <link rel="stylesheet" href="newsfeed/css/jquery.navgoco.css">
-    <link rel="stylesheet" href="newsfeed/css/flexslider.css">
-    <link rel="stylesheet" href="newsfeed/css/color-options.css" media="all">
-    <link rel="stylesheet" href="newsfeed/css/style.css">
-    <link rel="stylesheet" href="newsfeed/css/demo.css">
-    <link rel="stylesheet" href="newsfeed/css/responsive.css">
-    <script src="newsfeed/js/modernizr.custom.js"></script>
-    <link rel="stylesheet" href="newsfeed/css/skin/red.css" type="text/css" id="colors">
-    
-    <link rel="shortcut icon" href="newsfeed/img/favicon.ico">
-    <link rel="apple-touch-icon" href="newsfeed/img/apple-touch-icon.png">
-    <link rel="apple-touch-icon" sizes="72x72" href="newsfeed/img/apple-touch-icon-72x72.png">
-    <link rel="apple-touch-icon" sizes="114x114" href="newsfeed/img/apple-touch-icon-114x114.png">
+    <!-- Le fav and touch icons -->
+    <link rel="shortcut icon" href="img/favicon.ico">
+    <link rel="apple-touch-icon" href="img/apple-touch-icon.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="img/apple-touch-icon-72x72.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="img/apple-touch-icon-114x114.png">
 
     <!-- Google Fonts -->
-    <link href="http://fonts.googleapis.com/css?family=Raleway:400,300,700,600" rel="stylesheet" type="text/css">
-    <link href="http://fonts.googleapis.com/css?family=Lato:400,300italic,300,400italic,700,700italic" rel="stylesheet" type="text/css">
-
-    <style type="text/css">
-        .imgLiquid img {
-            visibility: hidden
-        }
-    </style>
+    <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,600' rel='stylesheet' type='text/css'>
+    <link href='http://fonts.googleapis.com/css?family=Lato:400,300italic,300,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
     {!! getcong('headcode') !!}
 
     @yield("header")
 
 </head>
-
+    
 <body class="punica-home-1">
-    <div id="theme-option" style="left: -275px;">
-        <div class="theme-opt-wrapper">
-            <p>
-                <em>You can use Unlimited Colors</em>
-            </p>
-            <ul class="choose-color">
-                <li>
-                    <a href="#" class="color red active">&nbsp;</a>
-                </li>
-                <li>
-                    <a href="#" class="color blue">&nbsp;</a>
-                </li>
-                <li>
-                    <a href="#" class="color cyan">&nbsp;</a>
-                </li>
-                <li>
-                    <a href="#" class="color pink">&nbsp;</a>
-                </li>
-                <li>
-                    <a href="#" class="color green">&nbsp;</a>
-                </li>
-                <li>
-                    <a href="#" class="color oran">&nbsp;</a>
-                </li>
-                <li>
-                    <a href="#" class="color purple">&nbsp;</a>
-                </li>
-            </ul>
-            <div class="text-center">
-                <a href="#" class="reset" onclick="return punica_theme_option_reset_CLICK();">Reset</a>
-            </div>
-        </div>
-        <!--end:theme-opt-wrapper-->
-        <a href="#" class="fa fa-cog open-close-button"> </a>
-        <!--open-close-button-->
-    </div>
-<?php $DB_USER_LANG = isset($DB_USER_LANG) ? $DB_USER_LANG : '' ?>
 
+<div id="theme-option">
+    <div class="theme-opt-wrapper">
+        <p><em>You can use Unlimited Colors</em></p>
+        <ul class="choose-color">
+            <li><a href="#" class="color red">&nbsp;</a></li>
+            <li><a href="#" class="color blue">&nbsp;</a></li>
+            <li><a href="#" class="color cyan">&nbsp;</a></li>
+            <li><a href="#" class="color pink">&nbsp;</a></li>
+            <li><a href="#" class="color green">&nbsp;</a></li>            
+            <li><a href="#" class="color oran">&nbsp;</a></li>
+            <li><a href="#" class="color purple">&nbsp;</a></li>
+        </ul>
+        <div class="text-center"><a href="#" class="reset" onClick="return punica_theme_option_reset_CLICK();">Reset</a></div>
+    </div><!--end:theme-opt-wrapper-->
+    <a href="#" class="fa fa-cog open-close-button"> </a><!--open-close-button-->
+</div><!--end:theme-option -->
+
+<?php $DB_USER_LANG = isset($DB_USER_LANG) ? $DB_USER_LANG : '' ?>
 @include("_particles.header")
 
 @yield("content")
 
 @include("_particles.footer")
 
-<script src="newsfeed/js/jquery-1.11.1.min.js"></script>
-<script src="newsfeed/js/bootstrap.min.js"></script>
-<script src="newsfeed/js/colorswitch.js"></script>
-<script src="newsfeed/js/custom.js" charset="utf-8"></script>
+<!-- end:punica-page-footer -->
+    
+<script src="js/jquery-1.11.1.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/colorswitch.js"></script>
+<script src="js/custom.js" charset="utf-8"></script>
 
 @yield("footer")
 @include('.errors.swalerror')
@@ -119,4 +84,5 @@
 {!!  getcong('footercode')  !!}
 
 </body>
+
 </html>
