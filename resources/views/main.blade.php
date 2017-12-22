3a9719a4411778627023289c7a9231acd200e709
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-23 02:20:55 +0800

    master themes app main meta

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -1,11 +1,28 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ Lang::getLocale() }}">
 <head>
     <meta charset="utf-8">
-    <title>UnicMag - HTML5 Responsive Template</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <title>@yield('head_title', getcong('sitetitle'))</title>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="@yield('head_description', getcong('sitemetadesc'))" />
+
+    <meta property="og:type" content="article" />
+    <meta property="og:title" content="@yield('head_title',  getcong('sitetitle'))" />
+    <meta property="og:description" content="@yield('head_description', getcong('sitemetadesc'))" />
+    
+    <meta property="og:image" content="@yield('head_image', url('/assets/img/flogo.png'))" />
+    <meta property="og:url" content="@yield('head_url', url())" />
+
+    <meta name="twitter:image" content="@yield('head_image', url('/assets/img/logo.png'))" />
+    <meta name="twitter:card" content="summary">
+    <meta name="twitter:url" content="@yield('head_url', url())">
+    <meta name="twitter:title" content="@yield('head_title',  getcong('sitetitle'))">
+    <meta name="twitter:description" content="@yield('head_description', getcong('sitemetadesc'))">
+    <link href='https://fonts.googleapis.com/css?family=Roboto+Condensed&subset=latin,cyrillic' rel='stylesheet' type='text/css'>
+    <link href="{{ url('/assets/img/favicon.png') }}" rel="shortcut icon" type="image/x-icon" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
     <link rel="stylesheet" href="css/bootstrap.min.css" media="all" />
     <link rel="stylesheet" href="css/font-awesome.min.css" media="all" />
     <link rel="stylesheet" href="css/superfish.css" media="all" />
@@ -18,7 +35,6 @@
     <link rel="stylesheet" href="demo.css">
     <link rel="stylesheet" href="css/responsive.css"/>
     <script src="js/modernizr.custom.js"></script>
-
 
     <!-- Color Switch -->
     <link rel="stylesheet" href="css/skin/red.css" type="text/css" id="colors" />
@@ -33,6 +49,11 @@
     <link href='http://fonts.googleapis.com/css?family=Raleway:400,300,700,600' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Lato:400,300italic,300,400italic,700,700italic' rel='stylesheet' type='text/css'>
 
+    <style type="text/css">
+        body {
+            font-family: 'Roboto Condensed', sans-serif;
+        }
+    </style>
     {!! getcong('headcode') !!}
 
     @yield("header")
